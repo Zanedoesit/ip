1ea5c78d7c0f8c67b40a950603ba359e49688e78
Zanedoesit <zanephuazh@outlook.com>
2026-01-29 00:03:05 +0800

    Renamed chatbot from Duke to Bob and added Rename,Greet and Exit code

---
+++ src/main/java/Bob.java
@@ -0,0 +1,24 @@
+/*
+This is the skeleton code for the BOB chatbot
+
+ */
+
+public class Bob {
+    public static void main(String[] args) {
+        String logo = " ____   ___   ____ \n"
+                + "| __ ) / _ \\ | __ )\n"
+                + "|  _ \\| | | ||  _ \\\n"
+                + "| |_) | |_| || |_) |\n"
+                + "|____/ \\___/ |____/ \n";
+
+/*        System.out.println("Hello from\n" + logo); */
+        System.out.println( "Hello from\n" + logo +
+                            "------------------------------------\n"
+                            + "Hi! I'm BOB\n"
+                            + "What can I do for you today?\n\n"
+                            + "------------------------------------\n"
+                            + "Bye. See you soon!\n\n"
+                            + "------------------------------------");
+
+    }
+}
