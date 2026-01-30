3bfabe063f3fc5de22823bb1b89b6332eb3c7624
Zanedoesit <zanephuazh@outlook.com>
2026-01-30 08:59:05 +0800

    Improved skeletal version of Bob so that it echos the user and exits when bye is typed in

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 This is the skeleton code for the BOB chatbot
 
@@ -10,15 +12,25 @@ public class Bob {
                 + "|  _ \\| | | ||  _ \\\n"
                 + "| |_) | |_| || |_) |\n"
                 + "|____/ \\___/ |____/ \n";
-
-/*        System.out.println("Hello from\n" + logo); */
+        /*Creates a scanner to read input from user */
+        Scanner scan = new Scanner(System.in);
         System.out.println( "Hello from\n" + logo +
                             "------------------------------------\n"
                             + "Hi! I'm BOB\n"
-                            + "What can I do for you today?\n\n"
-                            + "------------------------------------\n"
+                            + "What can I do for you today?\n\n");
+        /*Gets input from user to type and press Enter */
+        String input = scan.nextLine();
+        /*Checks for the bye message and ensures that it is not uppercase or lowercase sensitive */
+        if (input.toLowerCase().equals("bye")) {
+                           System.out.println("------------------------------------\n"
                             + "Bye. See you soon!\n\n"
-                            + "------------------------------------");
-
+                            + "\n------------------------------------");
+        }
+        /*Else, echo the user */
+        else{
+            System.out.println("------------------------------------\n"
+                                + input
+                                + "\n------------------------------------\n");
+        }
     }
 }
