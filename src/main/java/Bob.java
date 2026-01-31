8f2ba6f7462aa688cb88fe68d6ceec429e572319
Zanedoesit <zanephuazh@outlook.com>
2026-01-31 23:07:34 +0800

    Improved skeletal version for BOB and now that it echoes and only exits when user enters bye

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 /*
 This is the skeleton code for the BOB chatbot
 
@@ -7,30 +6,38 @@ This is the skeleton code for the BOB chatbot
 
 public class Bob {
     public static void main(String[] args) {
+        /* Status for loop to be used to keep the chat alive */
+        boolean status = true;
         String logo = " ____   ___   ____ \n"
-                + "| __ ) / _ \\ | __ )\n"
-                + "|  _ \\| | | ||  _ \\\n"
-                + "| |_) | |_| || |_) |\n"
+                + "|  ) / _ \\ |  )\n"
+                + "|  _ \\| | |   _ \\\n"
+                + "| |_) | |_|  |_) |\n"
                 + "|____/ \\___/ |____/ \n";
-        /*Creates a scanner to read input from user */
+
+        /* Creates a scanner to read input from user */
         Scanner scan = new Scanner(System.in);
-        System.out.println( "Hello from\n" + logo +
-                            "------------------------------------\n"
-                            + "Hi! I'm BOB\n"
-                            + "What can I do for you today?\n\n");
-        /*Gets input from user to type and press Enter */
-        String input = scan.nextLine();
-        /*Checks for the bye message and ensures that it is not uppercase or lowercase sensitive */
-        if (input.toLowerCase().equals("bye")) {
-                           System.out.println("------------------------------------\n"
-                            + "Bye. See you soon!\n\n"
-                            + "\n------------------------------------");
-        }
-        /*Else, echo the user */
-        else{
-            System.out.println("------------------------------------\n"
-                                + input
-                                + "\n------------------------------------\n");
+        /* Greeting message */
+        System.out.println("Hello from\n" + logo +
+                "------------------------------------\n"
+                + "Hi! I'm BOB\n"
+                + "What can I do for you today?\n\n");
+        /* Loop to keep chat alive until bye is entered */
+        while (status) {
+            /* Gets input from user */
+            String input = scan.nextLine();
+
+            if (input.toLowerCase().equals("bye")) {
+                System.out.println("------------------------------------\n"
+                        + "Bye. See you soon!\n\n"
+                        + "\n------------------------------------");
+                status = false;
+            }
+            /* Else, echo the user input */
+            else {
+                System.out.println("------------------------------------\n"
+                        + input
+                        + "\n------------------------------------\n");
+            }
         }
     }
 }
