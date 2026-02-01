e0d6beed0715bb9dc2bffc470fc07b350a7fe25e
Zanedoesit <zanephuazh@outlook.com>
2026-02-01 21:58:19 +0800

    Added try and catch for errors and checks, including out of range marks and invalid format

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -47,63 +47,122 @@ public class Bob {
                 System.out.println("------------------------------------");
             }
 
-            /* Check if user types in mark and gets the number of the marked task */
+            /* Check if user types in mark and gets the number of the
+            marked task and handle if number is out of range or invalid */
             else if (input.toLowerCase().startsWith("mark ")) {
-                int taskNumber = Integer.parseInt(input.substring(5)) - 1;
-                tasks.get(taskNumber).markAsDone();
+                try {
+                    int taskNumber = Integer.parseInt(input.substring(5)) - 1;
+                    if (taskNumber < 0 || taskNumber >= tasks.size()) {
+                        System.out.println("------------------------------------\n"
+                                + "ERROR! Task number is out of range please try again. \n"
+                                + "------------------------------------\n");
+                    }
+                    else{ /* mark task as done */
+                    tasks.get(taskNumber).markAsDone();
+                    System.out.println("------------------------------------\n"
+                            + "Nice! I've marked this task as done:\n"
+                            + tasks.get(taskNumber)
+                            + "\n------------------------------------\n");
+                }
+            } catch (NumberFormatException e){
                 System.out.println("------------------------------------\n"
-                        + "Nice! I've marked this task as done:\n"
-                        + tasks.get(taskNumber)
-                        + "\n------------------------------------\n");
+                        + "ERROR! Please enter a valid task number. \n"
+                        + "------------------------------------\n");
+            }
             }
 
             /* Else unmark and gets number of unmakred task */
-            else if(input.toLowerCase().startsWith("unmark ")) {
-                int taskNumber = Integer.parseInt(input.substring(7)) - 1;
-                tasks.get(taskNumber).markAsNotDone();
-                System.out.println("------------------------------------\n"
-                        + "Ok, I've marked this task as not done yet:\n"
-                        + tasks.get(taskNumber)
-                        + "\n------------------------------------\n");
-            }
-
-            else if (input.toLowerCase().startsWith("todo ")) {
-                String taskDescription = input.substring(5);
-                tasks.add(new ToDo(taskDescription));
-                System.out.println("------------------------------------\n"
-                        + "Got it. I've added this task:\n"
-                        + tasks.get(tasks.size()-1).toString() + "\n"
-                        + "Now you have " + Integer.toString(tasks.size()) + " tasks in the list.\n"
-                        + "------------------------------------\n");
-
-            } else if (input.toLowerCase().startsWith("deadline ")) {
-                String[] parts = input.substring(9).split(" /by ");
-                        if (parts.length == 2){
-                            String taskDescription = parts[0];
-                            String date = parts[1];
-                            tasks.add(new Deadline(taskDescription, date));
-                            System.out.println("------------------------------------\n"
-                                + "Got it. I've added this task:\n"
-                                +  tasks.get(tasks.size()-1).toString() + "\n"
-                                + "Now you have " + Integer.toString(tasks.size()) + " tasks in the list.\n"
+            else if (input.toLowerCase().startsWith("unmark ")) {
+                try {
+                    int taskNumber = Integer.parseInt(input.substring(7)) - 1;
+                    if (taskNumber < 0 || taskNumber >= tasks.size()) {
+                        System.out.println("------------------------------------\n"
+                                + "ERROR! Task number is out of range please try again. \n"
                                 + "------------------------------------\n");
-                        }
-            } else if (input.toLowerCase().startsWith("event ")) {
-                String[] parts = input.substring(6).split(" /from | /to ");
-                if (parts.length == 3){
-                    String taskDescription = parts[0];
-                    String start = parts[1];
-                    String end = parts[2];
-                    tasks.add(new Event (taskDescription, start , end));
+                    } else { /* add unmarked task */
+                        tasks.get(taskNumber).markAsNotDone();
+                        System.out.println("------------------------------------\n"
+                                + "Ok, I've marked this task as not done yet:\n"
+                                + tasks.get(taskNumber)
+                                + "\n------------------------------------\n");
+                    }
+                } catch (NumberFormatException e){
+                    System.out.println("------------------------------------\n"
+                            + "ERROR! Please enter a valid task number. \n"
+                            + "------------------------------------\n");
+                }
+                }
+
+            else if (input.toLowerCase().startsWith("todo")) {
+                String taskDescription = input.substring(4).trim();
+                /* check if task description has text behind */
+                if (taskDescription.isEmpty()){
+                    System.out.println("------------------------------------\n"
+                            + "ERROR! Please fill up the task todo. \n"
+                            + "------------------------------------\n");
+                }
+                else { /* If task has text add */
+                    tasks.add(new ToDo(taskDescription));
                     System.out.println("------------------------------------\n"
                             + "Got it. I've added this task:\n"
-                            + tasks.get(tasks.size()-1).toString() + "\n"
+                            + tasks.get(tasks.size() - 1).toString() + "\n"
                             + "Now you have " + Integer.toString(tasks.size()) + " tasks in the list.\n"
                             + "------------------------------------\n");
                 }
 
+            } else if (input.toLowerCase().startsWith("deadline ")) {
+                String[] parts = input.substring(9).split(" /by ");
+                        if (parts.length != 2){
+                            System.out.println("------------------------------------\n"
+                                    + "Error! Deadline format is wrong.\n"
+                                    + "Try: deadline <your task> by <date> \n"
+                                    + "------------------------------------\n");
+                        }
+                        else{ /* Check if task description and deadline is filled */
+                                String taskDescription = parts[0].trim();
+                                String date = parts[1].trim();
+                                if (taskDescription.isEmpty() || date.isEmpty()){
+                                    System.out.println("------------------------------------\n"
+                                            + "Error! Task description or deadline is empty"
+                                            + "Please fill it up"
+                                            + "------------------------------------\n");
+                                }
+                                else{
+                                    tasks.add(new Deadline(taskDescription, date));
+                                    System.out.println("------------------------------------\n"
+                                            + "Got it. I've added this task:\n"
+                                            + tasks.get(tasks.size() - 1).toString() + "\n"
+                                            + "Now you have " + Integer.toString(tasks.size()) + " tasks in the list.\n"
+                                            + "------------------------------------\n");
+                            }
+                            }
+                        }
+                else if (input.toLowerCase().startsWith("event ")) {
+                    String[] parts = input.substring(6).split(" /from | /to ");
+                    if (parts.length != 3) {
+                        System.out.println("------------------------------------\n"
+                                + "Error! Event format is wrong. \n"
+                                + "Try: event <your task> /from <start> /to <end> \n"
+                                + "------------------------------------\n");
+                    } else{ /* check for task description and start/end date */
+                        String taskDescription = parts[0].trim();
+                        String start = parts[1].trim();
+                        String end = parts[2].trim();
+                        if (taskDescription.isEmpty() || start.isEmpty() || end.isEmpty()){
+                            System.out.println("------------------------------------\n"
+                                    + "Error! Task description or start or end date should not be empty. \n"
+                                    + "------------------------------------\n");
+                        }
+                        else {
+                            tasks.add(new Event(taskDescription, start, end));
+                            System.out.println("------------------------------------\n"
+                                    + "Got it. I've added this task:\n"
+                                    + tasks.get(tasks.size() - 1).toString() + "\n"
+                                    + "Now you have " + Integer.toString(tasks.size()) + " tasks in the list.\n"
+                                    + "------------------------------------\n");
+                        }
+                    }
             }
-
             /* Else, add the task and print out what was added */
             else {
                 tasks.add(new Task(input));
