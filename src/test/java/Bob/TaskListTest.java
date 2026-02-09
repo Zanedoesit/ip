bfc3d0a34f0c302436f0e5705764e8b19821d5f1
Zanedoesit <zanephuazh@outlook.com>
2026-02-09 16:36:47 +0800

    Package the app as an executable JAR file

--- src/test/java/Bob/TaskListTest.java
+++ src/test/java/Bob/TaskListTest.java
@@ -1,4 +1,29 @@
 package Bob;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TaskListTest {
+
+    @Test
+    public void testAddTask() {
+        TaskList taskList = new TaskList();
+        Task task = new ToDo("read book");
+        taskList.addTask(task);
+        assertEquals(1, taskList.size());
+    }
+
+    @Test
+    public void testDeleteTask() {
+        TaskList taskList = new TaskList();
+        Task task1 = new ToDo("read book");
+        Task task2 = new ToDo("return book");
+        taskList.addTask(task1);
+        taskList.addTask(task2);
+        assertEquals(2, taskList.size());
+
+        taskList.deleteTask(0);
+        assertEquals(1, taskList.size());
+    }
+
 }
--- src/test/java/Bob/TaskTest.java
+++ src/test/java/Bob/TaskTest.java
@@ -1,4 +1,25 @@
 package Bob;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class TaskTest {
+
+    @Test
+    public void testMarkAsDone() {
+        Task task = new Task("test task");
+        assertFalse(task.isDone);
+        task.markAsDone();
+        assertTrue(task.isDone);
+    }
+
+    @Test
+    public void testGetStatusIcon() {
+        Task task = new Task("test task");
+        assertEquals("[]", task.getStatusIcon());
+        task.markAsDone();
+        assertEquals("[X]", task.getStatusIcon());
+    }
 }
